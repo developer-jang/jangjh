cace5e538dba575ce8a934b1ca1cf01027c8dd71
junhwan <developer.jangjh@gmail.com>
2018-12-05 23:33:48 +0900

    2일차 - application.properties 옵션 추가 - 모델 클래스 생성 및 구성

---
+++ src/main/java/com/example/springboot/jangjh/day2/domain/Board.java
@@ -0,0 +1,30 @@
+package com.example.springboot.jangjh.day2.domain;
+
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Data
+@Entity
+@Table(name = "tbl_boards")
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long bno;
+
+    private String title;
+
+    private String writer;
+
+    private String content;
+
+    @CreationTimestamp
+    private Timestamp regDate;
+
+    @UpdateTimestamp
+    private Timestamp updateDate;
+}

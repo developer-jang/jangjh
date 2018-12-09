e7eb5eb863d826dff6f44862ba2af4e12072d697
junhwan <developer.jangjh@gmail.com>
2018-12-10 00:53:14 +0900

    4일차 - spring-data-jpa 쿼리 추가 - target/ qdomain 생성 - dependency 추가(queryDsl)

--- src/main/java/com/example/springboot/jangjh/day3/repository/BoardRepository.java
+++ src/main/java/com/example/springboot/jangjh/day3/repository/BoardRepository.java
@@ -1,7 +1,12 @@
 package com.example.springboot.jangjh.day3.repository;
 
 import com.example.springboot.jangjh.day2.domain.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
+
 public interface BoardRepository extends CrudRepository<Board, Long> {
+
+     Page<Board> findByBnoGreaterThan(Long bno, Pageable pageable);
 }
--- src/test/java/com/example/springboot/jangjh/day3/BoardRepositoryTests.java
+++ src/test/java/com/example/springboot/jangjh/day3/BoardRepositoryTests.java
@@ -2,10 +2,14 @@ package com.example.springboot.jangjh.day3;
 
 import com.example.springboot.jangjh.day2.domain.Board;
 import com.example.springboot.jangjh.day3.repository.BoardRepository;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -16,6 +20,7 @@ public class BoardRepositoryTests {
     private BoardRepository boardRepository;
 
     @Test
+    @Ignore
     public void insert() {
         Board board = new Board();
         board.setTitle("Title1");
@@ -23,5 +28,13 @@ public class BoardRepositoryTests {
         board.setWriter("jangjh");
 
         boardRepository.save(board);
+    }
+
+    @Test
+    public void testBnoPagingSort() {
+        PageRequest paging = new PageRequest(0, 10, Sort.Direction.ASC, "bno");
+        Page<Board> result = boardRepository.findByBnoGreaterThan(0L, paging);
+
+
     }
 }

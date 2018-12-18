1b5d0f724f196e5c2280377cefd07221dec96527
junhwan <developer.jangjh@gmail.com>
2018-12-18 23:17:54 +0900

    - 지연로딩, 즉시로딩 - 인덱스

---
+++ src/main/java/com/example/springboot/jangjh/repository/FreeBoardReplyRepository.java
@@ -0,0 +1,7 @@
+package com.example.springboot.jangjh.repository;
+
+import com.example.springboot.jangjh.domain.FreeBoardReply;
+import org.springframework.data.repository.CrudRepository;
+
+public interface FreeBoardReplyRepository extends CrudRepository<FreeBoardReply, Long> {
+}
---
+++ src/main/java/com/example/springboot/jangjh/repository/FreeBoardRepository.java
@@ -0,0 +1,7 @@
+package com.example.springboot.jangjh.repository;
+
+import com.example.springboot.jangjh.domain.FreeBoard;
+import org.springframework.data.repository.CrudRepository;
+
+public interface FreeBoardRepository extends CrudRepository<FreeBoard, Long> {
+}
